a06a2f00731c4c7f1cca21d87cfe8b7680b12065
jschwind <info@juergen-schwind.de>
2023-11-26 13:04:44 +0100

    Moving

--- source/vendortools.serverlist/stable/oswtools/resources/php/configure/topmiddle/_vendor_classes.php
+++ source/vendortools.serverlist/stable/oswtools/resources/php/configure/topmiddle/_vendor_classes.php
@@ -1,21 +1,27 @@
-<?php
+<?php declare(strict_types=0);
+
+/**
+ * @var \osWFrame\Tools\Tool\Configure $this
+ */
 
 $this->settings = ['page_title' => 'Vendor Classes'];
 
 $vendor_settings = [];
 
-foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'oswvendor' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . '*') as $vendor) {
+foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'oswcore' . \DIRECTORY_SEPARATOR . 'oswvendor' . \DIRECTORY_SEPARATOR . 'classes' . \DIRECTORY_SEPARATOR . '*') as $vendor) {
     $vendor_name = strtolower(str_replace(['.'], ['_'], basename($vendor)));
-    $products = glob($vendor . DIRECTORY_SEPARATOR . 'version-*');
-    if ($products != []) {
-        $vendor_settings[$vendor_name] = [];
-        foreach (glob($vendor . DIRECTORY_SEPARATOR . '*') as $vendor_version) {
+    $products = glob($vendor . \DIRECTORY_SEPARATOR . 'version-*');
+    if ($products !== []) {
+        if (!isset($vendor_settings[$vendor_name])) {
+            $vendor_settings[$vendor_name] = [];
+        }
+        foreach (glob($vendor . \DIRECTORY_SEPARATOR . '*') as $vendor_version) {
             if (is_dir($vendor_version)) {
                 $vendor_version = basename($vendor_version);
                 $vendor_settings[$vendor_name][$vendor_version] = $vendor_version;
             }
         }
-        if ($vendor_settings[$vendor_name] == []) {
+        if ($vendor_settings[$vendor_name] === []) {
             unset($vendor_settings[$vendor_name]);
         } else {
             krsort($vendor_settings[$vendor_name]);
@@ -23,9 +29,30 @@ foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'osw
     }
 }
 
+foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'oswproject' . \DIRECTORY_SEPARATOR . 'oswvendor' . \DIRECTORY_SEPARATOR . 'classes' . \DIRECTORY_SEPARATOR . '*') as $vendor) {
+    $vendor_name = strtolower(str_replace(['.'], ['_'], basename($vendor)));
+    $products = glob($vendor . \DIRECTORY_SEPARATOR . 'version-*');
+    if ($products !== []) {
+        if (!isset($vendor_settings[$vendor_name])) {
+            $vendor_settings[$vendor_name] = [];
+        }
+        foreach (glob($vendor . \DIRECTORY_SEPARATOR . '*') as $vendor_version) {
+            if (is_dir($vendor_version)) {
+                $vendor_version = basename($vendor_version);
+                $vendor_settings[$vendor_name][$vendor_version] = $vendor_version;
+            }
+        }
+        if ($vendor_settings[$vendor_name] === []) {
+            unset($vendor_settings[$vendor_name]);
+        } else {
+            krsort($vendor_settings[$vendor_name]);
+        }
+    }
+}
+
+ksort($vendor_settings);
+
 foreach ($vendor_settings as $v => $vv) {
     $this->fields['vendor_class_' . $v . '_version'] = ['default_name' => $v, 'default_type' => 'select', 'default_select' => array_merge(['0' => 'newest'], $vv), 'default_value' => '', 'valid_type' => 'vendor', 'valid_min_length' => 1, 'valid_max_length' => 32, 'configure_write' => true];
     $this->fields['vendor_class_' . $v . '_versions'] = ['default_name' => $v . '_versions', 'default_type' => 'hidden', 'default_value' => implode(';', $vv), 'valid_type' => 'string', 'valid_min_length' => 1, 'valid_max_length' => 1024, 'configure_write' => true];
 }
-
-?>
--- source/vendortools.serverlist/stable/oswtools/resources/php/configure/topmiddle/_vendor_namespaces.php
+++ source/vendortools.serverlist/stable/oswtools/resources/php/configure/topmiddle/_vendor_namespaces.php
@@ -1,37 +1,46 @@
-<?php
+<?php declare(strict_types=0);
+
+/**
+ * @var \osWFrame\Tools\Tool\Configure $this
+ */
 
 $this->settings = ['page_title' => 'Vendor Namespaces'];
 
 $vendor_settings = [];
 
-foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'oswvendor' . DIRECTORY_SEPARATOR . 'namespaces' . DIRECTORY_SEPARATOR . '*') as $vendor) {
+foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'oswcore' . \DIRECTORY_SEPARATOR . 'oswvendor' . \DIRECTORY_SEPARATOR . 'namespaces' . \DIRECTORY_SEPARATOR . '*') as $vendor) {
     $vendor_name = strtolower(str_replace(['.'], ['_'], basename($vendor)));
-    $products = glob($vendor . DIRECTORY_SEPARATOR . 'version-*');
-    if ($products != []) {
-        foreach (glob($vendor . DIRECTORY_SEPARATOR . '*') as $vendor_version) {
+    $products = glob($vendor . \DIRECTORY_SEPARATOR . 'version-*');
+    if ($products !== []) {
+        if (!isset($vendor_settings[$vendor_name])) {
+            $vendor_settings[$vendor_name] = [];
+        }
+        foreach (glob($vendor . \DIRECTORY_SEPARATOR . '*') as $vendor_version) {
             if (is_dir($vendor_version)) {
                 $vendor_version = basename($vendor_version);
                 $vendor_settings[$vendor_name][$vendor_version] = $vendor_version;
             }
         }
-        if ($vendor_settings[$vendor_name] == []) {
+        if ($vendor_settings[$vendor_name] === []) {
             unset($vendor_settings[$vendor_name]);
         } else {
             krsort($vendor_settings[$vendor_name]);
         }
     } else {
-        foreach (glob($vendor . DIRECTORY_SEPARATOR . '*') as $product) {
+        foreach (glob($vendor . \DIRECTORY_SEPARATOR . '*') as $product) {
             $product_name = strtolower(str_replace(['.'], ['_'], basename($product)));
-            $vendor_versions = glob($product . DIRECTORY_SEPARATOR . 'version-*');
-            if ($vendor_versions != []) {
-                $vendor_settings[$vendor_name . '_' . $product_name] = [];
-                foreach (glob($product . DIRECTORY_SEPARATOR . '*') as $vendor_version) {
+            $vendor_versions = glob($product . \DIRECTORY_SEPARATOR . 'version-*');
+            if ($vendor_versions !== []) {
+                if (!isset($vendor_settings[$vendor_name . '_' . $product_name])) {
+                    $vendor_settings[$vendor_name . '_' . $product_name] = [];
+                }
+                foreach (glob($product . \DIRECTORY_SEPARATOR . '*') as $vendor_version) {
                     if (is_dir($vendor_version)) {
                         $vendor_version = basename($vendor_version);
                         $vendor_settings[$vendor_name . '_' . $product_name][$vendor_version] = $vendor_version;
                     }
                 }
-                if ($vendor_settings[$vendor_name . '_' . $product_name] == []) {
+                if ($vendor_settings[$vendor_name . '_' . $product_name] === []) {
                     unset($vendor_settings[$vendor_name . '_' . $product_name]);
                 } else {
                     krsort($vendor_settings[$vendor_name . '_' . $product_name]);
@@ -41,9 +50,51 @@ foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'osw
     }
 }
 
+foreach (glob(\osWFrame\Core\Settings::getStringVar('settings_framepath') . 'oswproject' . \DIRECTORY_SEPARATOR . 'oswvendor' . \DIRECTORY_SEPARATOR . 'namespaces' . \DIRECTORY_SEPARATOR . '*') as $vendor) {
+    $vendor_name = strtolower(str_replace(['.'], ['_'], basename($vendor)));
+    $products = glob($vendor . \DIRECTORY_SEPARATOR . 'version-*');
+    if ($products !== []) {
+        if (!isset($vendor_settings[$vendor_name])) {
+            $vendor_settings[$vendor_name] = [];
+        }
+        foreach (glob($vendor . \DIRECTORY_SEPARATOR . '*') as $vendor_version) {
+            if (is_dir($vendor_version)) {
+                $vendor_version = basename($vendor_version);
+                $vendor_settings[$vendor_name][$vendor_version] = $vendor_version;
+            }
+        }
+        if ($vendor_settings[$vendor_name] === []) {
+            unset($vendor_settings[$vendor_name]);
+        } else {
+            krsort($vendor_settings[$vendor_name]);
+        }
+    } else {
+        foreach (glob($vendor . \DIRECTORY_SEPARATOR . '*') as $product) {
+            $product_name = strtolower(str_replace(['.'], ['_'], basename($product)));
+            $vendor_versions = glob($product . \DIRECTORY_SEPARATOR . 'version-*');
+            if ($vendor_versions !== []) {
+                if (!isset($vendor_settings[$vendor_name . '_' . $product_name])) {
+                    $vendor_settings[$vendor_name . '_' . $product_name] = [];
+                }
+                foreach (glob($product . \DIRECTORY_SEPARATOR . '*') as $vendor_version) {
+                    if (is_dir($vendor_version)) {
+                        $vendor_version = basename($vendor_version);
+                        $vendor_settings[$vendor_name . '_' . $product_name][$vendor_version] = $vendor_version;
+                    }
+                }
+                if ($vendor_settings[$vendor_name . '_' . $product_name] === []) {
+                    unset($vendor_settings[$vendor_name . '_' . $product_name]);
+                } else {
+                    krsort($vendor_settings[$vendor_name . '_' . $product_name]);
+                }
+            }
+        }
+    }
+}
+
+ksort($vendor_settings);
+
 foreach ($vendor_settings as $v => $vv) {
     $this->fields['vendor_namespace_' . $v . '_version'] = ['default_name' => $v, 'default_type' => 'select', 'default_select' => array_merge(['0' => 'newest'], $vv), 'default_value' => '', 'valid_type' => 'vendor', 'valid_min_length' => 1, 'valid_max_length' => 32, 'configure_write' => true];
     $this->fields['vendor_namespace_' . $v . '_versions'] = ['default_name' => $v . '_versions', 'default_type' => 'hidden', 'default_value' => implode(';', $vv), 'valid_type' => 'string', 'valid_min_length' => 1, 'valid_max_length' => 1024, 'configure_write' => true];
 }
-
-?>
